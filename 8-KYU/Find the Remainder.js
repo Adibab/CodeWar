849bc9b8a30cff10189ab752c2c704556b677cd6
Adibab <adiba.du.bd@gmail.com>
2022-05-03 01:11:09 +0100

    Vowel Count

--- 8-KYU/Find the Remainder.js
+++ 8-KYU/Find the Remainder.js
@@ -40,6 +40,6 @@ function remainder(n, m){
     else {
       let answer = n%m;
       return answer;
-    }
+    }}
 
-// codewar link - https://www.codewars.com/kata/524f5125ad9c12894e00003f/train/javascript
+// codewar link - https://www.codewars.com/kata/524f5125ad9c12894e00003f/train/javascript//
