902eec2ce19a688fc90804200c654d6b4201f9e3
Adibab <adiba.du.bd@gmail.com>
2022-06-16 02:10:01 +0100

    16.06.22

---
+++ 8-KYU/Well of Ideas - Easy Version.js
@@ -0,0 +1,22 @@
+// For every good kata idea there seem to be quite a few bad ones!
+// In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
+
+
+function well(x){
+  
+    let goodCounter = 0;
+      for(let i = 0; i < x.length; i++){
+        if (x[i] === 'good') {
+           goodCounter++;
+      }}
+      
+      if(goodCounter > 2){
+        return 'I smell a series!';
+      } else if(goodCounter >= 1 && goodCounter <= 2){
+        return 'Publish!';
+      } else{ 
+        return 'Fail!';
+      }
+      }
+
+    //   codewar link - https://www.codewars.com/kata/57f222ce69e09c3630000212/train/javascript
