be1933aad318690cd20b587b56a1bc663fb4ead8
Adibab <adiba.du.bd@gmail.com>
2023-06-07 22:53:32 +0100

    Split Strings

---
+++ 6-KYU/Split Strings.js
@@ -0,0 +1,16 @@
+function solution(str) {
+  let arr = [];
+  let splitStr = str.split("");
+  // console.log(splitStr);
+  for (let i = 0; i < splitStr.length; i += 2) {
+    if (splitStr[i + 1]) {
+      arr.push(splitStr[i] + splitStr[i + 1]);
+    } else {
+      arr.push(splitStr[i] + "_");
+    }
+  }
+  return arr;
+}
+let input = "abc";
+
+console.log(solution(input));
