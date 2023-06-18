81d672f2ca7562eba8993a72e997267f4fd04357
Adibab <adiba.du.bd@gmail.com>
2023-06-19 00:30:08 +0100

    Multiples of 3 or 5

--- 6-KYU/Multiples of 3 or 5.js
+++ 6-KYU/Multiples of 3 or 5.js
@@ -9,4 +9,17 @@ function solution(number) {
   }
   return answer;
 }
+
+
+
+function solution(number) {
+  let newArray = [];
+  let answer = 0;
+  for (let i = 0; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      newArray.push(i);
+    }
+  }
+  return newArray.reduce((a, b) => a + b, 0);
+}
 https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript
