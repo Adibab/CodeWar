9490c9039383ba6fd5c17ac2bbcc2f337cbed217
Adibab <adiba.du.bd@gmail.com>
2023-07-16 23:37:30 +0100

    Update Persistent Bugger.js

--- 6-KYU/Persistent Bugger.js
+++ 6-KYU/Persistent Bugger.js
@@ -19,3 +19,26 @@ function persistence(num) {
 }
 let input = 999;
 console.log(persistence(input));
+
+
+function persistence(num) {
+  if (num.toString().length === 1) {
+    return 0;
+  }
+
+  let splitStr = num.toString().split("");
+  let count = 0;
+  let newlist;
+  while (splitStr.length > 1) {
+    newlist = splitStr.reduce((acc, cur) => {
+      return (acc *= cur);
+    });
+    console.log(newlist);
+    splitStr = newlist.toString().split("");
+    count++;
+  }
+
+  return count;
+}
+let input = 999;
+console.log(persistence(input));
