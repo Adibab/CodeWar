832115a295f7285252d46d154ec492b861f69bf0
Adibab <adiba.du.bd@gmail.com>
2023-07-31 23:54:55 +0100

    Moving Zeros To The End2

---
+++ .vscode/5 kyu/Moving Zeros To The End2.js
@@ -0,0 +1,30 @@
+function moveZeros(arr) {
+  let newArray = [];
+  let zeroArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      newArray.push(arr[i]);
+    } else {
+      zeroArray.push(arr[i]);
+    }
+  }
+
+  return newArray.concat(zeroArray);
+}
+function moveZeros(arr) {
+  let nonZeroArray = [];
+  let zeroArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      nonZeroArray.push(arr[i]);
+    } else {
+      zeroArray.push(arr[i]);
+    }
+  }
+  return nonZeroArray.concat(zeroArray);
+}
+let input = [false, 1, 0, 1, 2, 0, 1, 3, "a"];
+let input2 = [1, 2, 0, 1, 0, 1, 0, 3, 0, 1];
+
+console.log(moveZeros(input));
+console.log(moveZeros(input2));
